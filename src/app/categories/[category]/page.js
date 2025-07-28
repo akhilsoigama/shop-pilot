65c8fc98bb8e8619805a3aa838997de578506c66
dharmikvarsani <dharmikvarsani968@gmail.com>
2025-07-28 13:51:18 +0530

    Show product from header and fix dark mode

--- src/app/categories/[category]/page.js
+++ src/app/categories/[category]/page.js
@@ -1,19 +1,21 @@
 'use client'
+
 import { Subcategories } from '@/lib/category'
 import { useRouter, useParams } from 'next/navigation'
 
 export default function SubcategoryPage() {
   const router = useRouter()
   const { category } = useParams()
-
   const decodedCategory = decodeURIComponent(category)
   const subcatObj = Subcategories.find(sc => sc.name === decodedCategory)
 
-  if (!subcatObj) return <div>No Subcategories Found</div>
+  if (!subcatObj) return <div className="text-gray-800 dark:text-gray-100">No Subcategories Found</div>
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">{decodedCategory} Subcategories</h1>
+    <div className="p-4 pt-[80px]">
+      <h1 className="text-xl font-bold mb-4 text-gray-900 dark:text-white">
+        {decodedCategory} Subcategories
+      </h1>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {subcatObj.subcategories.map((subcat) => (
           <button
@@ -21,7 +23,7 @@ export default function SubcategoryPage() {
             onClick={() =>
               router.push(`/categories/${encodeURIComponent(decodedCategory)}/${encodeURIComponent(subcat)}`)
             }
-            className="p-4 bg-blue-100 rounded hover:bg-blue-200"
+            className="p-4 rounded transition-colors bg-blue-100 hover:bg-blue-200 dark:bg-[#1a1832] dark:hover:bg-[#272455] text-gray-900 dark:text-gray-100"
           >
             {subcat}
           </button>
--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,4 +1,5 @@
 'use client'
+
 import { categories } from "@/lib/category"
 import { useRouter } from "next/navigation"
 
@@ -6,16 +7,21 @@ export default function CategoriesPage() {
   const router = useRouter()
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
-      {categories.map((cat) => (
-        <button
-          key={cat}
-          onClick={() => router.push(`/categories/${encodeURIComponent(cat)}`)}
-          className="p-4 bg-gray-200 rounded hover:bg-gray-300"
-        >
-          {cat}
-        </button>
-      ))}
-    </div>
+    <>
+      <h1 className="text-xl font-bold mb-4 pt-[80px] ps-10 text-gray-900 dark:text-white">
+        All Categories
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4">
+        {categories.map((cat) => (
+          <button
+            key={cat}
+            onClick={() => router.push(`/categories/${encodeURIComponent(cat)}`)}
+            className="p-4 rounded transition-colors bg-gray-200 hover:bg-gray-300 dark:bg-[#131226] dark:hover:bg-[#1d1c36] text-gray-800 dark:text-gray-100"
+          >
+            {cat}
+          </button>
+        ))}
+      </div>
+    </>
   )
 }
