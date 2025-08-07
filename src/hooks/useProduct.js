90bbe16cf808d76de8730cb3d3fc9da116527c27
dharmikvarsani <dharmikvarsani968@gmail.com>
2025-08-07 09:37:47 +0530

    changes in category and subcategory

--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -1,37 +1,3 @@
-// import axios from 'axios';
-// import useSWR from 'swr';
-
-// const fetcher = (url) => axios.get(url).then((res) => res.data);
-
-// export function useProducts(category = '', subcategory = '') {
-//   // Convert to encoded URI components
-//   const encodedCategory = encodeURIComponent(category);
-//   const encodedSubcategory = encodeURIComponent(subcategory);
-  
-//   // Build URL
-//   const basePath = process.env.NEXT_PUBLIC_ADMIN_API || '/api/product';
-//   const url = `${basePath}?category=${encodedCategory}&subcategory=${encodedSubcategory}`;
-
-//   const { data, error, isLoading, isValidating } = useSWR(
-//     subcategory ? url : null, 
-//     fetcher,
-//     {
-//       revalidateOnFocus: false,
-//       refreshInterval: 30000,
-//       shouldRetryOnError: true,
-//       errorRetryInterval: 5000,
-//     }
-//   );
-
-//   return {
-//     products: data || [],
-//     isLoading,
-//     isError: error,
-//     isValidating,
-//   };
-// }
-
-
 import axios from 'axios';
 import useSWR from 'swr';
 
