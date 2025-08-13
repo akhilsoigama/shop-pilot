cdab6470721e2e2903f4a3d923f27af7395b2ba8
akhil Soigama <akhilsoigama@gmail.com>
2025-08-13 16:46:30 +0530

    change cart style and header changes

--- src/components/subcategory/Subcategory.jsx
+++ src/components/subcategory/Subcategory.jsx
@@ -134,7 +134,7 @@ export default function Subcategory() {
             animate={{ opacity: 1 }}
             className="bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-950 min-h-screen"
         >
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 md:py-12 lg:py-16">
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8  lg:py-16">
                 <motion.div
                     initial={{ y: -20, opacity: 0 }}
                     animate={{ y: 0, opacity: 1 }}
